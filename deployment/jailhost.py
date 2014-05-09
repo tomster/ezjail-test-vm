a54a7819a3e294402e9c21fb075f6ac10c026918
Tom Lazar <tomster@pyfidelity.com>
2014-05-09 14:19:10 +0200

    adjust to https://github.com/tomster/bsdploy/commit/3572bd3aac928e2ac04923aca0c7feff5257654a

--- deployment/jailhost.py
+++ deployment/jailhost.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from fabric.api import env
-from bsdploy.fabric import bootstrap, fetch_assets
+from bsdploy.fabrics import bootstrap, fetch_assets
 
 # shutup pyflakes
 (bootstrap, fetch_assets)
